5e1fcbe35b03786fd289c8fafbc8bba6233a3baf
AlexanderOsinin <95582654+AlexanderOsinin@users.noreply.github.com>
2025-04-21 21:08:46 +0300

    Update Main.java
    Some change!

--- src/main/java/hw_1/qa/Main.java
+++ src/main/java/hw_1/qa/Main.java
@@ -2,6 +2,6 @@ package hw_1.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's test!");
+        System.out.println("Let's test more!!!!");
     }
 }
