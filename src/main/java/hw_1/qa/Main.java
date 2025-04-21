6c5fecece8261ac94cdaa56bccd99d47afde3747
Aleksandr <alexander1996osinin@mail.ru>
2025-04-21 21:10:33 +0300

    Add new feature

--- src/main/java/hw_1/qa/Main.java
+++ src/main/java/hw_1/qa/Main.java
@@ -1,7 +1,11 @@
 package hw_1.qa;
 
+import java.sql.SQLOutput;
+import java.util.SortedMap;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Let's test!");
+        System.out.println("You need to write more tests!");
     }
 }
